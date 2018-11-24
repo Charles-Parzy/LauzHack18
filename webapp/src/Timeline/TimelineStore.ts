931bc02d5e57fea7f5671aba68b5bfc93cfe4717
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2018-11-24 15:59:50 +0100

    Define the mobx store of the timeline component.

--- webapp/src/Timeline/TimelineStore.ts
+++ webapp/src/Timeline/TimelineStore.ts
@@ -1,3 +1,20 @@
-export default class TimelineStore {
+/* tslint:disable */
+import { computed, observable } from "mobx";
 
+export default class TimelineStore {
+    @observable private _followedProjects: Project[] = [];
+    @computed get followedProjects(): Project[] {
+        return this._followedProjects;
+    }
+    set followedProjects(followedProjects: Project[]) {
+        return this._followedProjects;
+    }
+
+    @observable private _recommendedProjects: Project[] = [];
+    @computed get recommendedProjects(): Project[] {
+        return this._recommendedProjects;
+    }
+    set recommendedProjects(recommendedProjects: Project[]) {
+        this._recommendedProjects = recommendedProjects;
+    }
 }
