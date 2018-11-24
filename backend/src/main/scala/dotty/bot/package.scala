a4d2a4489edb1d91df1940b2dd27d1f8e093036f
MikaelMorales <morales.mikael@outlook.com>
2018-11-24 20:14:30 +0100

    Timeline recommended repos

--- backend/src/main/scala/dotty/bot/package.scala
+++ backend/src/main/scala/dotty/bot/package.scala
@@ -3,6 +3,6 @@ package dotty
 import java.net.HttpURLConnection.{HTTP_BAD_REQUEST, HTTP_OK}
 
 package object bot {
-  def BadRequest(msg: String) = cask.Response(msg, HTTP_BAD_REQUEST)
-  def Ok(msg: String) = cask.Response(msg, HTTP_OK)
+  def BadRequest(msg: String) = cask.Response(msg, HTTP_BAD_REQUEST, headers = Seq(("Access-Control-Allow-Origin", "*")))
+  def Ok(msg: String) = cask.Response(msg, HTTP_OK, headers = Seq(("Access-Control-Allow-Origin", "*")))
 }
