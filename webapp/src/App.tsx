8e56d72b8874ffb3fb4ca0d0c358bf386b54a8d6
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2018-11-24 12:14:12 +0100

    [webapp] Add material-ui dependencies.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,3 +1,4 @@
+import Button from '@material-ui/core/Button';
 import * as React from 'react';
 import './App.css';
 
@@ -14,6 +15,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button>Default</Button>
       </div>
     );
   }
